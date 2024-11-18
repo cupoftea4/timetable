a58b4bb1311d465d9f18797ebfa247a613fc0692
cupoftea4 <nadiia.telko.pz.2021@lpnu.ua>
2024-11-18 15:50:53 +0200

    [chore] gtag event source

--- src/hooks/useGTagTimetableEvents.ts
+++ src/hooks/useGTagTimetableEvents.ts
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 
 const useGTagTimetableEvents = (group: string, source: string, isCustomSource?: boolean) => {
+  // biome-ignore lint/correctness/useExhaustiveDependencies: it should be sent only when the group changes
   useEffect(() => {
     window.gtag("event", "open_timetable", {
       event_category: "Timetable",
       event_label: group,
       group,
+      source,
     });
   }, [group]);
 
@@ -15,6 +17,7 @@ const useGTagTimetableEvents = (group: string, source: string, isCustomSource?: 
       event_label: source ?? "url",
       isCustomSource,
       group,
+      source,
     });
   }, [source, isCustomSource, group]);
 };
